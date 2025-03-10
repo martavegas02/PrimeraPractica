3281935f7b822cb6983edffa60fe3f4bd8da55b9
martavegas02 <martitavegas286@gmail.com>
2025-03-10 20:59:35 +0100

    Club Deportivo Alto Rendimiento 100%, falta el BeforeEach

--- src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
+++ src/test/java/clubdeportivo/ClubDeportivoAltoRendimientoTest.java
@@ -4,10 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ClubDeportivoAltoRendimientoTest {
+
+    @BeforeEach
+    public void setUp() {
+        
+    }
     
     @Test
     @DisplayName("Comprobar el constructor que el numero maximo de personas por grupo no sea <=0")
